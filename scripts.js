a4a7922d20930d160e1d33eab1a44bebd1897749
Marko Velgre <mvelgre98@gmail.com>
2023-10-28 20:48:10 +0300

    Miljon muudatust - mdea

--- scripts.js
+++ scripts.js
@@ -17,3 +17,29 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// Your new JavaScript code here
+
+var images = ["Midjourney_guitar_1.png", "Midjourney_shirt_1.png", "Midjourney_shirt_2.png",
+ "Midjourney_shirt_3.png", "Midjourney_randomsticker_1.png", "nisuhennu.png"]; // Replace with your image names
+
+var descriptions = ["Lambi kitarr", "Kollane särk", "Sinine särk", "Punane särk", "Lambi kleepekas", "Nisu Henry"]; // Replace with your descriptions
+
+var container = document.getElementById('merch-images');
+
+for (var i = 0; i < images.length; i++) {
+    var imgDiv = document.createElement('div');
+    imgDiv.className = 'merch-image';
+
+    var img = document.createElement('img');
+    img.src = 'images/merch/' + images[i];
+    img.alt = 'Merch ' + (i + 1);
+    imgDiv.appendChild(img);
+
+    var p = document.createElement('p');
+    p.textContent = descriptions[i];
+    p.className = 'image-description'; // Add this line
+    imgDiv.appendChild(p);
+
+    container.appendChild(imgDiv);
+}
